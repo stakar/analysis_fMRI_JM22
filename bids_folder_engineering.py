5a6b20afd88f251f4af15f044cb73897cab646cc
Stanisław Karkosz <34490120+stakar@users.noreply.github.com>
2023-11-30 19:56:14 +0100

    Updated converting files, seperated eng script

---
+++ bids_folder_engineering.py
@@ -0,0 +1,38 @@
+from os import listdir
+import re
+from bids_jm22_tools import setup_bids_dirs, move_file
+
+PATH = 'source_data/'
+DESTINATION = 'bids/'
+if __name__ == '__main__':
+
+    dirs = [n for n in listdir(PATH) if 'JM22' in n]
+    
+    
+    for directory in dirs:
+        code = re.search(r"B\d+",directory)[0]
+    
+        if len(code)>0:
+            setup_bids_dirs(code,DESTINATION)
+            print('Setup of bids is done!')
+        
+        
+            for session,timepoint in zip([1,2],[1,5]):
+                for extension in ['nii','json']:
+                    for modality,label,task,suffix in zip(
+                            ['anat','func','func','FieldMapAP','FieldMapPA'],
+                            ['T1w','run-01','run-02','fmap','fmap'],
+                            ['','_imagery','_imagery','',''],
+                            ['','_bold','_bold','_epi','_epi']
+                            ):
+                
+                        move_file(session,
+                                  timepoint,
+                                  extension,
+                                  modality,
+                                  label,
+                                  task,
+                                  suffix,
+                                  code=code,
+                                  old_path_prefix=PATH,
+                                  new_path_prefix=DESTINATION)
--- convert_files.py
+++ convert_files.py
@@ -1,7 +1,7 @@
 import subprocess
 from os import listdir, rename
+from shutil import move
 import re
-import bids_jm22_tools
 
 if __name__ == '__main__':
     zipped = [n for n in listdir() if 'zip' in n]
@@ -11,37 +11,17 @@ if __name__ == '__main__':
     #Renaming files
     try:
         dirs = [n for n in listdir() if 'JM22' in n]
+        
         for directory in dirs:
-            to_rename = listdir(f"{directory}")
-            timepoint = re.search(r"TP\d",directory)[0]
-            rename(f"{directory}/{to_rename[0]}",f"{directory}/{timepoint}")
-            subprocess.call(["dcm2niix",f"{directory}/{to_rename[0]}",f"{directory}/{timepoint}"])
+            move(directory,'source_data/')
+            to_rename = listdir(f"source_data/{directory}")
+            timepoint = re.search(r"TP\d",f"source_data/{directory}")[0]
+            rename(f"source_data/{directory}/{to_rename[0]}",
+                   f"source_data/{directory}/{directory}")
+            subprocess.call(["dcm2niix",
+                             f"source_data/{directory}/{directory}",
+                             f"source_data/{directory}/{directory}"])
     except Exception as e:
         print(e)
-        
-    code = re.search(r"B\d+",dirs[0])[0]
 
-    setup_bids_dirs(code)
-
-
-    for session,timepoint in zip([1,2],[1,5]):
-        move_file(session,timepoint,'json','anat','T1w')
-        move_file(session,timepoint,'nii','anat','T1w')
-        move_file(session,timepoint,'json','func','run-01','_imagery','_bold')
-        move_file(session,timepoint,'nii','func','run-01','_imagery','_bold')
-        move_file(session,timepoint,'json','func','run-02','_imagery','_bold')
-        move_file(session,timepoint,'nii','func','run-02','_imagery','_bold')
-
-        move_file(session,timepoint,'json','anat','T1w')
-        move_file(session,timepoint,'nii','anat','T1w')
-
-        move_file(session,timepoint,'json','func','run-01','_imagery','_bold')
-        move_file(session,timepoint,'nii','func','run-01','_imagery','_bold')
-        move_file(session,timepoint,'json','func','run-02','_imagery','_bold')
-        move_file(session,timepoint,'nii','func','run-02','_imagery','_bold')
-
-        move_file(session,timepoint,'nii','FieldMapPA')
-        move_file(session,timepoint,'json','FieldMapPA')
-
-        move_file(session,timepoint,'nii','FieldMapAP')
-        move_file(session,timepoint,'json','FieldMapAP')
+    print('Conversion dcm to nifti is done.')   
