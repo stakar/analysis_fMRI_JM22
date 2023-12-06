b8ce05c01bccf6f8fac9843e529d53309a66c534
Stanisław Karkosz <34490120+stakar@users.noreply.github.com>
2023-12-06 14:12:15 +0100

    Added exception handler
    Added exception handler to code that moves nifti to BIDS directory, as some files are wrongly named due to human error and need to be omited

--- bids_folder_engineering.py
+++ bids_folder_engineering.py
@@ -10,29 +10,34 @@ if __name__ == '__main__':
     
     
     for directory in dirs:
-        code = re.search(r"B\d+",directory)[0]
-    
-        if len(code)>0:
-            setup_bids_dirs(code,DESTINATION)
-            print('Setup of bids is done!')
+        try:
+            code = re.search(r"B\d+",directory)[0]
         
-        
-            for session,timepoint in zip([1,2],[1,5]):
-                for extension in ['nii','json']:
-                    for modality,label,task,suffix in zip(
-                            ['anat','func','func','FieldMapAP','FieldMapPA'],
-                            ['T1w','run-01','run-02','fmap','fmap'],
-                            ['','_imagery','_imagery','',''],
-                            ['','_bold','_bold','_epi','_epi']
-                            ):
+            if len(code)>0:
+                setup_bids_dirs(code,DESTINATION)
+                print(f'Setup of bids for {code} is done!')
+            
+            
+                for session,timepoint in zip([1,2],[1,5]):
+                    for extension in ['nii','json']:
+                        for modality,label,task,suffix in zip(
+                                ['anat','func','func','FieldMapAP','FieldMapPA'],
+                                ['T1w','run-01','run-02','fmap','fmap'],
+                                ['','_imagery','_imagery','',''],
+                                ['','_bold','_bold','_epi','_epi']
+                                ):
+                    
+                            move_file(session,
+                                      timepoint,
+                                      extension,
+                                      modality,
+                                      label,
+                                      task,
+                                      suffix,
+                                      code=code,
+                                      old_path_prefix=PATH,
+                                      new_path_prefix=DESTINATION)
                 
-                        move_file(session,
-                                  timepoint,
-                                  extension,
-                                  modality,
-                                  label,
-                                  task,
-                                  suffix,
-                                  code=code,
-                                  old_path_prefix=PATH,
-                                  new_path_prefix=DESTINATION)
+        except Exception as e:
+            print(e)
+            print(f'Creation of BIDS failed for {directory}')
