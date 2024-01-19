2db5a5d7d66bc86a9b385b3966d99dbaf3418b8e
Stanisław Karkosz <34490120+stakar@users.noreply.github.com>
2024-01-19 13:43:31 +0100

    Updated comments
    Updated comments with information which part of code needs to be changed if problem with one subject occured and you need to run for one subject instead of everyone.

--- run_bids.py
+++ run_bids.py
@@ -13,13 +13,17 @@ PATH = 'sourcedata/'
 
 if __name__ == '__main__':
     #search for directories with data from study
-    dirs = [n for n in listdir(PATH) if 'JM2' in n]
+    dirs = [n for n in listdir(PATH) if 'JM' in n]
+    #if any of the bids needs fixing after running whole command,
+    #you may want to change "JM" into subject code (eg. B999), 
+    #to run command only for concrete subject. 
+    print(dirs)
     #for each directory, find subject's code (exception handling
     #for files where is no subject code or time point), 
     #each subject code is B + 3 digits
     #each time point is either 1 or 5
     #then run dcm2bids command
-    for directory in dirs:
+    for directory in dirs: 
         try: 
             subject = re.search(r"B\d+",directory)[0]
             timepoint = re.search(r"TP\d",directory)[0][-1]
@@ -32,7 +36,8 @@ if __name__ == '__main__':
                              "-s",
                              timepoint, 
                              "-c",
-                             "code/jm22a_config.json"])
+                             "code/jm22a_config.json",
+                             "--auto_extract_entities"])
         except Exception as e:
             print(e)
             print(f"Problem with {directory}")
