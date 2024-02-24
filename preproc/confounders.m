def055b718d371222b253804485df20feebb38d2
Stanisław Karkosz <34490120+stakar@users.noreply.github.com>
2024-02-24 19:37:22 +0100

    Update confounders.m
    Added original file info

--- preproc/confounders.m
+++ preproc/confounders.m
@@ -1,3 +1,4 @@
+% Code modified, original from https://github.com/nencki-lobi/neurogrieg/tree/main/code/preproc/bids-matlab
 addpath('bids-matlab-main');
 datadir = fullfile(pwd, './'); % fmriprep dataset location
 resdir = fullfile(pwd, './'); % output location
