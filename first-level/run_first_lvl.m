83303be950f61165b66b54d00afa39af8932dfd4
Stanisław Karkosz <34490120+stakar@users.noreply.github.com>
2024-03-05 10:51:01 +0100

    Changed specification of triggers directory

--- first-level/run_first_lvl.m
+++ first-level/run_first_lvl.m
@@ -10,9 +10,10 @@
 
 workdir = pwd;
 basedir = fullfile(pwd, './'); % git repo location
-datadir = fullfile(pwd, 'spm12_test/'); % fmriprep dataset location
-confounddir = fullfile(pwd, 'spm12_test/results/');
-resdir = fullfile(pwd, 'results2/'); % output location
+datadir = fullfile(pwd, '../derivatives/'); % fmriprep dataset location
+confounddir = fullfile(pwd, '../derivatives/confounds/');
+resdir = fullfile(pwd, '../results/'); % output location
+triggdir = fullfile(pwd, '../triggers/');
 
 D = dir(fullfile(datadir,'sub-*'));
 D = D([D.isdir]);
@@ -32,7 +33,6 @@ for crun = 1:nrun
 
     sub = subjects{crun};
 
-
     if ~isfolder(fullfile(resdir, sub, 'stories-model'))
         % If it doesn't exist, create the directory
         mkdir(fullfile(resdir, sub, 'stories-model'))
@@ -43,24 +43,20 @@ for crun = 1:nrun
     inputs{1, crun} = {fullfile(resdir, sub, 'stories-model/')}; % fMRI model specification: Directory - cfg_files
     
     inputs{2, crun} = {fullfile(datadir, sub,'ses-1', 'func', ['s6', sub, '_ses-1_task-fmri_run-01_', bold, '.nii'])}; % fMRI model specification: Scans - cfg_files
-    inputs{3, crun} = {fullfile(basedir, 'triggers', sub,'ses-1', [sub, '_ses-1_Run1.mat'])}; % fMRI model specification: Multiple conditions - cfg_files
+    inputs{3, crun} = {fullfile(triggdir, sub,'ses-1', [sub, '_ses-1_Run1.mat'])}; % fMRI model specification: Multiple conditions - cfg_files
     inputs{4, crun} = {fullfile(confounddir, sub, 'stats', [sub, '_ses-1_task-fmri_run-01_confounds.mat'])}; % fMRI model specification: Multiple regressors - cfg_files
     
     inputs{5, crun} = {fullfile(datadir, sub,'ses-1', 'func', ['s6', sub, '_ses-1_task-fmri_run-02_', bold, '.nii'])}; % fMRI model specification: Scans - cfg_files
-    inputs{6, crun} = {fullfile(basedir, 'triggers', sub,'ses-1', [sub, '_ses-1_Run2.mat'])}; % fMRI model specification: Multiple conditions - cfg_files
+    inputs{6, crun} = {fullfile(triggdir, sub,'ses-1', [sub, '_ses-1_Run2.mat'])}; % fMRI model specification: Multiple conditions - cfg_files
     inputs{7, crun} = {fullfile(confounddir, sub, 'stats', [sub, '_ses-1_task-fmri_run-02_confounds.mat'])}; % fMRI model specification: Multiple regressors - cfg_files
     
     inputs{8, crun} = {fullfile(datadir, sub,'ses-2', 'func', ['s6', sub, '_ses-2_task-fmri_run-01_', bold, '.nii'])}; % fMRI model specification: Scans - cfg_files
-    inputs{9, crun} = {fullfile(basedir, 'triggers', sub,'ses-2', [sub, '_ses-2_Run1.mat'])}; % fMRI model specification: Multiple conditions - cfg_files
+    inputs{9, crun} = {fullfile(triggdir, sub,'ses-2', [sub, '_ses-2_Run1.mat'])}; % fMRI model specification: Multiple conditions - cfg_files
     inputs{10, crun} = {fullfile(confounddir, sub, 'stats', [sub, '_ses-2_task-fmri_run-01_confounds.mat'])}; % fMRI model specification: Multiple regressors - cfg_files
     
     inputs{11, crun} = {fullfile(datadir, sub,'ses-2', 'func', ['s6', sub, '_ses-2_task-fmri_run-02_', bold, '.nii'])}; % fMRI model specification: Scans - cfg_files
-    inputs{12, crun} = {fullfile(basedir, 'triggers', sub,'ses-2', [sub, '_ses-2_Run2.mat'])}; % fMRI model specification: Multiple conditions - cfg_files
+    inputs{12, crun} = {fullfile(triggdir, sub,'ses-2', [sub, '_ses-2_Run2.mat'])}; % fMRI model specification: Multiple conditions - cfg_files
     inputs{13, crun} = {fullfile(confounddir, sub, 'stats', [sub, '_ses-2_task-fmri_run-02_confounds.mat'])}; % fMRI model specification: Multiple regressors - cfg_files
-    
-    % inputs{5, crun} = {fullfile(datadir, sub, 'func', ['s6', sub, '_task-stories_run-02_', bold, '.nii'])}; % fMRI model specification: Scans - cfg_files
-    % inputs{6, crun} = {fullfile(basedir, 'triggers', sub,'ses-1', [sub, '_ses-1_Run2.m'])}; % fMRI model specification: Multiple conditions - cfg_files
-    % inputs{7, crun} = {fullfile(resdir, sub, 'stats', [sub, '_task-stories_run-02_confounds.mat'])}; % fMRI model specification: Multiple regressors - cfg_files
     
 end
 
