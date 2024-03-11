8e338bc0cf23cbca83c947d643907abed7d39d5d
Stanisław Karkosz <34490120+stakar@users.noreply.github.com>
2024-03-11 12:26:13 +0100

    Create run_second_lvl_ttest_job.m
    Added first attempt of scritping second level, this is job file for spm

---
+++ second-level/run_second_lvl_ttest_job.m
@@ -0,0 +1,15 @@
+%-----------------------------------------------------------------------
+% Job saved on 06-Mar-2024 15:23:42 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
