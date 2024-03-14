d7851abd48bb381222dd4823d326cccde8b7bb8b
Stanisław Karkosz <34490120+stakar@users.noreply.github.com>
2024-03-14 19:58:45 +0100

    Make directory undefined (nec. for multiple conditions)

--- second-level/run_second_lvl_ttest_job.m
+++ second-level/run_second_lvl_ttest_job.m
@@ -1,9 +1,9 @@
 %-----------------------------------------------------------------------
-% Job saved on 06-Mar-2024 17:21:32 by cfg_util (rev $Rev: 7345 $)
+% Job saved on 14-Mar-2024 18:20:08 by cfg_util (rev $Rev: 7345 $)
 % spm SPM - SPM12 (7771)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
-matlabbatch{1}.spm.stats.factorial_design.dir = {'E:\SPM_test\results_2ndLvl\T-Test_crit_l_sus'};
+matlabbatch{1}.spm.stats.factorial_design.dir = '<UNDEFINED>';
 matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = '<UNDEFINED>';
 matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
 matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
@@ -17,7 +17,7 @@ matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specific
 matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
 matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
 matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
-matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'T_test_Crit_l_sus';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = '<UNDEFINED>';
 matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = 1;
 matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
 matlabbatch{3}.spm.stats.con.delete = 0;
